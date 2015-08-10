f9958cf25df3b174551494499972fc55a7f63d8c
Karthigak <karthiga.k@optisolbusiness.com>
2015-08-10 17:43:22 +0530

    Initial Commit

---
+++ sam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sam
+//
+//  Created by OBS_Macmini on 8/10/15.
+//  Copyright (c) 2015 OptisolBusinessSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
