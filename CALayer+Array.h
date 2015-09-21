6dc31c7e49a3e955b9072170a4d8f33e945ffd8e
Karthigak <karthiga.k@optisolbusiness.com>
2015-09-21 14:13:10 +0530

    save

---
+++ CALayer+Array.h
@@ -0,0 +1,13 @@
+//
+//  CALayer+Array.h
+//  sam
+//
+//  Created by OBS_Macmini on 8/10/15.
+//  Copyright (c) 2015 OptisolBusinessSolutions. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface CALayer (Array)
+
+@end
---
+++ CALayer+Array.m
@@ -0,0 +1,13 @@
+//
+//  CALayer+Array.m
+//  sam
+//
+//  Created by OBS_Macmini on 8/10/15.
+//  Copyright (c) 2015 OptisolBusinessSolutions. All rights reserved.
+//
+
+#import "CALayer+Array.h"
+
+@implementation CALayer (Array)
+
+@end
